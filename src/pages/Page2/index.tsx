461b1a9d18cab4d75b875472799ed7673d4571f3
zkp <442910864@qq.com>
2025-01-10 18:07:41 +0800

    feat: 添加Page2组件，使用useStateData管理状态；在Page1中添加计数按钮

--- src/pages/Page2/index.tsx
+++ src/pages/Page2/index.tsx
@@ -1,6 +1,6 @@
 import { useStateData } from '@/hooks';
 
-const Page1 = () => {
+const Page2 = () => {
 
     const { state, update, } = useStateData(() => ({
         count: 0,
@@ -8,7 +8,7 @@ const Page1 = () => {
 
     return (
         <>
-            页面1
+            页面2
             <button onClick={() => {
                 state.count++;void update();
             }}>count: {state.count}</button>
@@ -16,4 +16,4 @@ const Page1 = () => {
     );
 };
 
-export default Page1;
+export default Page2;
