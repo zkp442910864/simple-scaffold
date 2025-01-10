2b138ef9f73370c384dac13e33afdf86101d9389
zkp <442910864@qq.com>
2025-01-10 21:03:03 +0800

    feat: 更新微应用配置，根据环境动态设置子应用入口地址；添加对.vite的忽略

--- src/qiankun/master.ts
+++ src/qiankun/master.ts
@@ -1,14 +1,26 @@
 import { loadMicroApp } from 'qiankun';
 
-export const microAppConfig: TMicroAppConfigConfig[] = [
-    {
-        name: 'child1',
-        entry: '//localhost:7100',
-    },
-    {
-        name: 'child2',
-        entry: '//localhost:7101',
-    },
-];
+export const microAppConfig: TMicroAppConfigConfig[] =
+    import.meta.env
+        ? [
+            {
+                name: 'child1',
+                entry: '//localhost:7100',
+            },
+            {
+                name: 'child2',
+                entry: '//localhost:7101',
+            },
+        ]
+        : [
+            {
+                name: 'child1',
+                entry: 'https://zkp442910864.github.io/simple-scaffold/child-app1',
+            },
+            {
+                name: 'child2',
+                entry: '//localhost:7101',
+            },
+        ];
 
 export type TMicroAppConfigConfig = Partial<Parameters<typeof loadMicroApp>[0]> & {name: string, entry: string};
