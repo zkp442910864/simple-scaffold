65ec5ce6b6362c80467ea476d94d847f1671e51c
zkp <442910864@qq.com>
2025-01-15 16:34:38 +0800

    feat: 添加样式文件并更新 Storybook 配置，优化文档生成脚本

--- lib/config/vite-plugin-typedoc/index.ts
+++ lib/config/vite-plugin-typedoc/index.ts
@@ -1,9 +1,18 @@
-import { exec, execSync } from 'node:child_process';
-import { existsSync, fsync, glob, globSync } from 'node:fs';
+import { exec } from 'node:child_process';
+import { existsSync, globSync } from 'node:fs';
 import path from 'node:path';
 import { Plugin } from 'vite';
 
+
 const watchCache: Record<string, boolean> = {};
+const pluginUrl = path.join(__dirname, '../typedoc-plugin-custom-output/index.js');
+
+/**
+ *
+ * @param pathStr
+ * @param isWatch 开启后 卡死了
+ * @returns
+ */
 const generateMarkDown = (pathStr: string, isWatch = false) => {
   if (!pathStr.match(/index(.tsx|.ts)$/)) return;
   const dirPath = path.dirname(pathStr);
@@ -11,6 +20,8 @@ const generateMarkDown = (pathStr: string, isWatch = false) => {
 
   if (!existsSync(storiesPath)) return;
   if (isWatch && watchCache[pathStr]) return;
+
+  console.log('vite-plugin-typedoc:handle');
 
   watchCache[pathStr] = true;
   const command = [
@@ -23,9 +34,17 @@ const generateMarkDown = (pathStr: string, isWatch = false) => {
     '--hidePageTitle true',
     '--hidePageHeader true',
     '--disableSources true',
+    '--interfacePropertiesFormat table',
+    '--parametersFormat table',
+    '--enumMembersFormat table',
+    '--typeDeclarationFormat table',
+    '--propertiesFormat table',
     '--plugin typedoc-plugin-markdown',
+    `--plugin ${pluginUrl}`,
     isWatch ? '--watch' : '',
   ];
+
+  // console.log(command.join(' '));
 
   exec(command.join(' '), (error, stdout, stderr) => {
     if (error) {
@@ -52,13 +71,13 @@ export default function () {
       isDev = env.command === 'serve';
     },
     configureServer(server) {
-      server.watcher.on('add', (str) => generateMarkDown(str, true));
-      server.watcher.on('addDir', (str) => generateMarkDown(str, true));
-      server.watcher.on('change', (str) => generateMarkDown(str, true));
+      server.watcher.on('add', (str) => generateMarkDown(str, false));
+      server.watcher.on('addDir', (str) => generateMarkDown(str, false));
+      server.watcher.on('change', (str) => generateMarkDown(str, false));
     },
     buildStart: () => {
       if (isDev) {
-        firstGenerateAllMarkDown(true);
+        firstGenerateAllMarkDown(false);
       }
     },
     // renderStart: () => {
--- lib/scripts/generateTemplate.ts
+++ lib/scripts/generateTemplate.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const globalMap: TMap = {
   component: {
-    url: path.join(__dirname, '../src/component'),
+    url: path.join(__dirname, '../src/components'),
     handleName(name: string) {
       return name.replace(/\w/, (val) => val.toLocaleUpperCase());
     },
@@ -85,112 +85,8 @@ async function getBaseResult(map: TMap) {
   return { configItem, result: mergeResult, };
 };
 
-/** 生成 hooks 模板 */
-function generateHooksTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {
-  const baseUrl = configItem.url;
-  const templateName = result.name;
-
-  const urls = [
-    {
-      url: path.join(baseUrl, templateName),
-      type: 'folder',
-    },
-    {
-      url: path.join(baseUrl, templateName, 'index.ts'),
-      type: 'file',
-      content: `
-
-        export const ${templateName} = () => {};
-
-      `,
-    },
-    {
-      url: path.join(baseUrl, templateName, 'index.type.ts'),
-      type: 'file',
-      content: '',
-    },
-    {
-      url: path.join(baseUrl, templateName, 'stories'),
-      type: 'folder',
-    },
-    {
-      url: path.join(baseUrl, templateName, 'stories', 'README.md'),
-      type: 'file',
-      content: '',
-    },
-    {
-      url: path.join(baseUrl, templateName, 'stories', `${templateName}.mdx`),
-      type: 'file',
-      content: `
-
-        import { Meta, Controls, Markdown, Story, Canvas } from '@storybook/blocks';
-        import * as all from './${templateName}.stories';
-        import Readme from './README.md?raw';
-
-        <Meta of={all} />
-
-        # Default
-
-        <Canvas of={all.Default} sourceState="shown" source={all.Default.source} />
-
-        <Markdown>{Readme}</Markdown>
-
-      `,
-    },
-    {
-      url: path.join(baseUrl, templateName, 'stories', `${templateName}.stories.tsx`),
-      type: 'file',
-      content: `
-        import { Meta, StoryObj } from '@storybook/react';
-        import { ${templateName} } from '..';
-        import { useState } from 'react';
-
-        // 具体情况,看想要什么参数进行可以受控,然后再下面args中进行配置
-        const Template = (props: Parameters<typeof ${templateName}>[0]) => {
-
-          return (
-            <div>
-              ...
-            </div>
-          );
-        };
-
-        const meta: Meta = {
-          title: 'Utils/Hooks/${templateName}',
-          component: Template,
-          // More on argTypes: https://storybook.js.org/docs/api/argtypes
-          argTypes: {},
-          parameters: {
-            // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-            layout: 'centered',
-          },
-          // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-          // tags: ['autodocs',],
-          // Use \`fn\` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-          args: {
-            interval: 500,
-            immediate: false,
-          },
-        };
-
-        type Story = StoryObj<typeof meta>;
-
-        export const Default = Template.bind({});
-        (Default as unknown as {source: {transform: (code: string) => string}}).source = {
-          transform: (() => {
-            let cacheCode = '';
-            return (code: string) => {
-              if (!cacheCode) cacheCode = code;
-              return cacheCode;
-            };
-          })(),
-        };
-
-        export default meta;
-      `,
-    },
-  ];
-
+/** 创建文件 */
+function createFiles(urls: ({url: string; type: string; content?: undefined;} | {url: string; type: string; content: string;})[], baseUrl: string, templateName: string) {
   urls.forEach(({ type, url, content, }) => {
     if (fs.existsSync(url)) return;
 
@@ -202,14 +98,249 @@ function generateHooksTemplate({ configItem, result, }: Awaited<ReturnType<typeo
       fs.writeFileSync(url, newContent, { encoding: 'utf-8', });
     }
   });
-  fs.appendFileSync(path.join(baseUrl, 'index.ts'), `\nexport * from './${templateName}';`);
+}
+
+/** 更新index */
+function updateIndexFile(baseUrl: string, templateName: string) {
+  fs.appendFileSync(path.join(baseUrl, '../', 'index.ts'), `\nexport * from './${templateName}';`);
+}
+
+/** 生成 hooks 模板 */
+function generateHooksTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {
+  const templateName = result.name;
+  const firstUpperTplName = result.name.replace(/\w/, (val) => val.toLocaleUpperCase());;
+  const baseUrl = path.join(configItem.url, templateName);
+
+  const urls = [
+    {
+      url: path.join(baseUrl),
+      type: 'folder',
+    },
+    {
+      url: path.join(baseUrl, 'index.ts'),
+      type: 'file',
+      content: `
+        export * from './index.type';
+        import type { I${firstUpperTplName}Data } from './index.type';
+
+        export const ${templateName} = (data: I${firstUpperTplName}Data) => {};
+
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'index.type.ts'),
+      type: 'file',
+      content: `
+        interface I${firstUpperTplName}Data {
+          xxx: string;
+        }
+
+        export type {
+          I${firstUpperTplName}Data
+        };
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'stories'),
+      type: 'folder',
+    },
+    {
+      url: path.join(baseUrl, 'stories', 'README.md'),
+      type: 'file',
+      content: '',
+    },
+    {
+      url: path.join(baseUrl, 'stories', `${templateName}.mdx`),
+      type: 'file',
+      content: `
+
+        import { Meta, Controls, Markdown, Story, Canvas } from '@storybook/blocks';
+        import * as all from './${templateName}.stories';
+        import Readme from './README.md?raw';
+
+        <Meta of={all} />
+
+        <Canvas of={all.Default} sourceState="shown" source={all.Default.source} />
+
+        <Markdown>{Readme}</Markdown>
+
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'stories', `${templateName}.stories.tsx`),
+      type: 'file',
+      content: `
+        import { Meta, StoryObj } from '@storybook/react';
+        import { ${templateName} } from '..';
+
+        type Story = StoryObj<Meta>;
+
+        // 具体情况,看想要什么参数进行可以受控,然后再下面args中进行配置
+        const Template = (props: Parameters<typeof ${templateName}>[0]) => {
+
+          return (
+            <div>
+              {props.xxx}
+            </div>
+          );
+        };
+
+        export const Default = Template.bind({});
+        (Default as unknown as {source: {transform: (code: string) => string}}).source = {
+          transform: (() => {
+            let cacheCode = '';
+            return (code: string) => {
+              if (!cacheCode) cacheCode = code;
+              return cacheCode;
+            };
+          })(),
+        };
+        (Default as Story).args = {
+          xxx: 'asss',
+        };
+
+        export default {
+          title: 'Utils/Hooks/${templateName}',
+          component: Template,
+          // More on argTypes: https://storybook.js.org/docs/api/argtypes
+          argTypes: {},
+          parameters: {
+            // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+            // layout: 'centered',
+          },
+          // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+          // tags: ['autodocs',],
+          // Use \`fn\` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+          // args: {},
+        } satisfies Meta;
+      `,
+    },
+  ];
+
+  createFiles(urls, baseUrl, templateName);
+  updateIndexFile(baseUrl, templateName);
 }
 
 /** 生成 component 模板 */
-function generateComponentTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {}
+function generateComponentTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {
+  const templateName = result.name;
+  const baseUrl = path.join(configItem.url, templateName);
+
+  const urls = [
+    {
+      url: path.join(baseUrl),
+      type: 'folder',
+    },
+    {
+      url: path.join(baseUrl, 'index.tsx'),
+      type: 'file',
+      content: `
+        export * from './index.type';
+        import { FC } from 'react';
+        import type { I${templateName}Props } from './index.type';
+
+        export const ${templateName}: FC<I${templateName}Props> = ({
+          xxx,
+        }) => {
+
+          return (
+            <>
+              {xxx}
+            </>
+          );
+        };
+
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'index.type.ts'),
+      type: 'file',
+      content: `
+        interface I${templateName}Props {
+          xxx: string;
+        }
+
+        export type {
+          I${templateName}Props
+        };
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'stories'),
+      type: 'folder',
+    },
+    {
+      url: path.join(baseUrl, 'stories', 'README.md'),
+      type: 'file',
+      content: '',
+    },
+    {
+      url: path.join(baseUrl, 'stories', `${templateName}.mdx`),
+      type: 'file',
+      content: `
+
+        import { Meta, Controls, Markdown, Story, Canvas, Docs, ArgTypes } from '@storybook/blocks';
+        import * as all from './${templateName}.stories';
+        import Readme from './README.md?raw';
+
+        <Meta of={all} />
+
+        <Canvas sourceState="shown"/>
+
+        <ArgTypes/>
+
+        <Markdown>{Readme}</Markdown>
+
+      `,
+    },
+    {
+      url: path.join(baseUrl, 'stories', `${templateName}.stories.tsx`),
+      type: 'file',
+      content: `
+        import type { Meta, StoryObj } from '@storybook/react';
+        import { fn } from '@storybook/test';
+        import { ${templateName} } from '..';
+
+        // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+        export default {
+          title: 'Component/${templateName}',
+          component: ${templateName},
+          parameters: {
+            // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+            // layout: 'centered',
+          },
+          // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+          // tags: ['autodocs',],
+          // More on argTypes: https://storybook.js.org/docs/api/argtypes
+          argTypes: {},
+          // Use \`fn\` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+          // args: { onClick: fn(), },
+        } satisfies Meta<typeof ${templateName}>;
+
+        type Story = StoryObj<Meta<typeof ${templateName}>>;
+
+        // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+        export const Default: Story = {
+          args: {
+            xxx: '12123',
+          },
+        };
+
+
+      `,
+    },
+  ];
+
+  createFiles(urls, baseUrl, templateName);
+  updateIndexFile(baseUrl, templateName);
+}
 
 /** 生成 function 模板 */
-function generateFunctionTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {}
+function generateFunctionTemplate({ configItem, result, }: Awaited<ReturnType<typeof getBaseResult>>) {
+  // const templateName = result.name;
+  // const baseUrl = path.join(configItem.url, templateName);
+  generateHooksTemplate({ configItem, result, });
+}
 
 
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
