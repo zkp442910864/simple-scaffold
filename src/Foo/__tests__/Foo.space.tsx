125bdef8f6f3c0b3f8fcee984272cb561ba445fe
zkp <442910864@qq.com>
2025-03-17 23:41:21 +0800

    feat: 添加 Jest 测试配置和测试用例，更新 .gitignore 以排除测试文件

---
+++ src/Foo/__tests__/Foo.space.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { it } from 'node:test';
+import React from 'react';
+import { Foo } from '../Foo';
+
+it('', () => {
+  render(
+    <Foo
+      title22323222sd2ssds232={''}
+      c333={{
+        aaa: {
+          title22323222sd2ssds232: '',
+          c333: 0,
+          b322: '',
+          s333: '',
+          abc: {
+            aaa: undefined,
+          },
+        },
+      }}
+      b322={{
+        abc: {},
+        title22323222sd2ssds232: '',
+        s333: 'undefined',
+      }}
+    />,
+  );
+});
